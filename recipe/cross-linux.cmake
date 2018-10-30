37633a4f6d0a0f82b6699effe4700667c0d10048
Aaron Ang <awz.ang@gmail.com>
2018-10-29 22:59:38 -0700

    Add system dependencies

--- recipe/cross-linux.cmake
+++ recipe/cross-linux.cmake
@@ -9,8 +9,12 @@ set(CMAKE_CXX_COMPILER $ENV{CXX})
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
 
+message("PREFIX: $ENV{PREFIX}")
+message("BUILD_PREFIX: $ENV{BUILD_PREFIX}")
+message("HOST: $ENV{HOST}")
+
 # search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 # for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
